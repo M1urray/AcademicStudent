3013d67211b8364f55f471ed15cbc50be8f7d819
M1urray <rknjonjo@gmail.com>
2024-01-09 10:42:44 +0300

    changes to site master and reference maps

--- New Student Portal/Properties/Settings.Designer.cs
+++ New Student Portal/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace New_Student_Portal.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace New_Student_Portal.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://erp.dsl.ke:7333/MIBS/WS/MIBS/Codeunit/WebPortal")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://erp.dsl.ke:3031/MIBS/WS/MIBS/Codeunit/WebPortal")]
         public string New_Student_Portal_NAVWS_WebPortal {
             get {
                 return ((string)(this["New_Student_Portal_NAVWS_WebPortal"]));
